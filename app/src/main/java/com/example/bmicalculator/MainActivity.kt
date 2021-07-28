3ba11472e1e3dacf75b1ac7dc50adaaf19315f33
amitcode77 <amitcode77@gmail.com>
2021-07-28 20:41:32 +0530

    Code of my first session

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        submitBtn.setOnClickListener {
+            var weight: Double = weightTxt.text.toString().toDouble()
+            var height: Double = weightTxt.text.toString().toDouble()
+            var bmi = weight/(height*height)
+            resultText.text = "Your Body mass Index is $bmi"
+
+            if(bmi>23)
+                Toast.makeText(this,"Your are overWeight",Toast.LENGTH_SHORT).show()
+
+
+        }
+
+    }
+}
